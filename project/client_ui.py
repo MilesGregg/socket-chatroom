7961a05470db39b6f054a97015034d6a306b2736
Miles <milesgregg3@gmail.com>
2021-06-28 16:26:16 -0600

    Update client_ui.py

--- project/client_ui.py
+++ project/client_ui.py
@@ -6,25 +6,68 @@ class ClientUIWidget(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent=parent)
 
-        self.layout = QVBoxLayout(self)
+        self.window_layout = QVBoxLayout(self)
         self.tabs = QTabWidget()
-        self.tabs.resize(300,200)        
         self.home_tab = QWidget()
-        self.tab2 = QWidget()
-        self.tabs.addTab(self.tab1, "Connect")
-        self.tabs.addTab(self.tab2, "Chat")
-        self.tabs.setTabEnabled(1,False)
-        
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)  
+        self.chat_tab = QWidget()
+        self.tabs.addTab(self.home_tab, "Connect")
+        self.tabs.addTab(self.chat_tab, "Chat")
+        self.tabs.setTabEnabled(1, False)
+
+        self.set_home_tab(home_tab=self.home_tab)
+
+        self.window_layout.addWidget(self.tabs)
+        self.setLayout(self.window_layout)
+
+    def set_home_tab(self, home_tab: QWidget):
+        grid = QGridLayout()
+        home_tab.setLayout(grid)
+
+        # nickname
+        self.nickname = QLabel(self)
+        self.nickname.setText('Nickname:')
+        self.nickname_input = QLineEdit(self)
+        self.nickname_input.setStyleSheet("background-color:white")
+        grid.addWidget(self.nickname, 0, 0, 1, 1)
+        grid.addWidget(self.nickname_input, 0, 1, 1, 1)
+
+        # ip address
+        self.ip_address = QLabel(self)
+        self.ip_address.setText('IP Address:')
+        self.ip_address_input = QLineEdit(self)
+        self.ip_address_input.setStyleSheet("background-color:white")
+        grid.addWidget(self.ip_address, 1, 0, 1, 1)
+        grid.addWidget(self.ip_address_input, 1, 1, 1, 1)
+
+        # port
+        self.port = QLabel(self)
+        self.port.setText('Port:')
+        self.port_input = QLineEdit(self)
+        self.port_input.setStyleSheet("background-color:white")
+        grid.addWidget(self.port, 2, 0, 1, 1)
+        grid.addWidget(self.port_input, 2, 1, 1, 1)
+
+        # connect button
+        self.connect = QPushButton("Connect")
+        self.connect.setStyleSheet("background-color:rgb(25, 106, 255)")
+        self.disconnect = QPushButton("Disconnect")
+        self.disconnect.setStyleSheet("background-color:rgb(25, 106, 255)")
+        grid.addWidget(self.connect, 3, 0, 1, 1)
+        grid.addWidget(self.disconnect, 3, 1, 1, 1)
+
+        grid.setColumnStretch(0, 1)
+        grid.setColumnStretch(1, 1)
+
+    def set_chat_tab(self, chat_tab: QWidget):
+        grid = QGridLayout()
 
 
-class ClientUI(QMainWindow): 
+class ClientUI(QMainWindow):
     def __init__(self):
         super(ClientUI, self).__init__()
 
         self.setWindowTitle("client")
-        self.setGeometry(100, 100, 500, 500)
+        self.setGeometry(100, 100, 400, 400)
         self.setCentralWidget(ClientUIWidget(self))
         self.show()
 
@@ -67,5 +110,6 @@ def set_dark_theme():
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    set_dark_theme()
     c = ClientUI()
     sys.exit(app.exec_())
